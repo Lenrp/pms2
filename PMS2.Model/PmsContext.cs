b501db2a12c818541aee0bf5452b900b516f7140
Lenard Painchaud <len@doublespresso.ca>
2020-05-09 12:05:16 -0600

    Add project files.

---
+++ PMS2.Model/PmsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace PMS2.Model
+{
+    public class PmsContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connStr = ConfigurationManager.AppSettings["connect"];
+            options.UseSqlServer(connStr);
+        }
+    }                   
+}
